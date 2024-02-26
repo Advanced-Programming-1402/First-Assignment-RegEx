11b4e8b6472725fb2e130e83031cb30ea641b1c8
AmirAli <amirnasiri1383710@gmail.com>
2024-02-27 00:31:16 +0330

    complete Assignment 3

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -11,7 +11,7 @@ public class Exercise3 {
     */
 
     public static String extractURL(String text) {
-        String regex = "write your regex pattern here!";  // TODO
+        String regex = "http[s]?:.+\\..+[mro]";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(text);
@@ -29,18 +29,54 @@ public class Exercise3 {
      */
 
     public static boolean validateEmail(String email) {
-        // TODO
-        return false;
+        String regex = "\\w+@\\w*\\.\\w+[mg]";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.find()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /*
     implement the method below so that it returns a list of words with repeated letters
     */
 
+    private static boolean isItRepeatedLetter(String str) { // Function to check a word is RepeatedLetter or not
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j < str.length(); j++) {
+                if (str.charAt(i) == str.charAt(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     public static List<String> findWordsWithRepeatLetters(String input) {
         List<String> wordsWithRepeatLetters = new ArrayList<>();
+        List<String> tempList = new ArrayList<>(); // contains all of input Words
+
+        String wordRegex = "\\w+";
+
+        Pattern pattern = Pattern.compile(wordRegex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            tempList.add(matcher.group(0));
+        }
+
+        for (String s : tempList) {
+            if (isItRepeatedLetter(s)) {
+                wordsWithRepeatLetters.add(s);
+            }
+        }
+
         return wordsWithRepeatLetters;
-        // TODO
     }
 
     /*
@@ -49,10 +85,39 @@ public class Exercise3 {
     for example: "appleapple orange pearpear pineapple" -> ["appleapple", "pearpear"]
     */
 
+    private static boolean isItRepeatedWord(String s) { // Function to check a word is RepeatedWord or not
+        int border = s.length() / 2;
+        String sub1 = s.substring(0, border);
+        String sub2 = s.substring(border, s.length());
+
+        if (sub1.equals(sub2)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
     public static List<String> findReapetdWords(String input) {
         List<String> repeatedWords = new ArrayList<>();
+        List<String> tempList = new ArrayList<>(); // contains all of input Words
+
+        String wordRegex = "\\w+";
+
+        Pattern pattern = Pattern.compile(wordRegex);
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            tempList.add(matcher.group(0));
+        }
+
+        for (String str : tempList) {
+            if (isItRepeatedWord(str)) {
+                repeatedWords.add(str);
+            }
+        }
+
         return repeatedWords;
-        // TODO
     }
 
     public static void main(String[] args) {
